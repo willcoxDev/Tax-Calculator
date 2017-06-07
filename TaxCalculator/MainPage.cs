53e27dca0c70872ced3b21b343aa1970e499f3d1
laptop - Rick <rickwillcoxau@outlook.com>
2017-06-07 12:37:31 +1000

    HW visible

--- TaxCalculator/MainPage.cs
+++ TaxCalculator/MainPage.cs
@@ -15,7 +15,8 @@ namespace TaxCalculator
         public MainPage()
         {
             InitializeComponent();
-           
+            txtHoursWork.Visible = false; // setting the hours worked info to not be visible on program start up
+            lblHoursWork.Visible = false;
         }
 
         private void btnCreateEmployeeForm_Click(object sender, EventArgs e)
@@ -30,7 +31,17 @@ namespace TaxCalculator
 
         private void txtEmployeeID_TextChanged(object sender, EventArgs e)
         {
-            txtHoursWork.Visible = false;
+            if(txtEmployeeID.Text.ToLower().StartsWith("e")) // checking to see if the employee ID starts with e or E. 
+            {
+                txtHoursWork.Visible = true; //sett the hours worked info to visible if true.
+                lblHoursWork.Visible = true;
+            }
+            else
+            {
+                txtHoursWork.Visible = false; //removes the hours worked if they delete the e to enter a new ID.
+                lblHoursWork.Visible = false;
+            }
+            
         }
     }
 }
